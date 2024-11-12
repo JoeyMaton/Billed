db281eb0cc077078ef9863228aa9abe8d425df14
Joey Maton <jmaton52@outlook.fr>
2024-11-12 11:55:02 +0100

    v0.9

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -165,6 +165,39 @@ describe("Given I am connected as an employee", () => {
       // On s'assure qu'on est bien renvoyé sur la page Bills
       expect(screen.getByText(/Mes notes de frais/i)).toBeVisible();
     });
+
+    test("Then a new bill should be created", async () => {
+      const createBill = jest.fn(mockStore.bills().create);
+      const updateBill = jest.fn(mockStore.bills().update);
+
+      const { fileUrl, key } = await createBill();
+
+      expect(createBill).toHaveBeenCalledTimes(1);
+
+      expect(key).toBe("1234");
+      expect(fileUrl).toBe("https://localhost:3456/images/test.jpg");
+
+      const newBill = updateBill();
+
+      expect(updateBill).toHaveBeenCalledTimes(1);
+
+      await expect(newBill).resolves.toEqual({
+        id: "47qAXb6fIm2zOKkLzMro",
+        vat: "80",
+        fileUrl:
+          "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+        status: "pending",
+        type: "Hôtel et logement",
+        commentary: "séminaire billed",
+        name: "encore",
+        fileName: "preview-facture-free-201801-pdf-1.jpg",
+        date: "2004-04-04",
+        amount: 400,
+        commentAdmin: "ok",
+        email: "a@a",
+        pct: 20,
+      })
+    })
   })
 })
 
